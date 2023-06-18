7093b1ca32fd600aa2cb221eb93542c167483e2d
Orsi Suto <orsolya.suto.so@gmail.com>
2023-06-18 17:20:52 +0200

    init commit

---
+++ pages/game.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Game() {
+    return (
+        <>
+            <Link href="/">BACK</Link>
+            <h1>GAME PAGE</h1>
+        </>
+    );
+}
