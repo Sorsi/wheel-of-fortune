325f9571367c23af908cb5248aa60934284063db
Orsi Suto <orsolya.suto.so@gmail.com>
2023-06-22 16:35:08 +0200

    resolved merge conflicts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
 
 async function main() {
 	await prisma.user.create({
 		data: {
 			name: 'sorsi',
-			points: 666666,
 		},
 	});
 }
